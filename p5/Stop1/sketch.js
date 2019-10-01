ccd91368bee46215b6086788bced139ad874c631
Brandon Thomas <thomas_brandon77@yahoo.com>
2019-10-01 13:42:51 -0500

    yessss

---
+++ p5/Stop1/sketch.js
@@ -0,0 +1,53 @@
+var myState = 0;
+var timer = 200;
+
+
+function setup() {
+  // put setup code here
+  createCanvas(800, 800);
+  rectMode(CENTER);
+  ellipseMode(CENTER);
+}
+
+function draw() {
+  // put drawing code here
+  background(100);
+  fill('#F7FF3C');
+  rect(width / 2, height / 2, 150, 400);
+
+  switch (myState) {
+    case 0: // green state
+      fill('grey');
+      ellipse(width / 2, height / 2 - 125, 100, 100); // top circle
+      ellipse(width / 2, height / 2, 100, 100); // middle
+
+      fill('green');
+      ellipse(width / 2, height / 2 + 125, 100, 100); // bottom
+
+
+      break;
+
+    case 1: // yellow state      fill('grey');
+
+      fill('grey');
+      ellipse(width / 2, height / 2 - 125, 100, 100); // top circle
+      ellipse(width / 2, height / 2 + 125, 100, 100); // bottom
+
+      fill('yellow');
+      ellipse(width / 2, height / 2, 100, 100); // middle
+
+      break;
+
+    case 2: // red state
+      fill('red');
+      ellipse(width / 2, height / 2 - 125, 100, 100); // top circle
+
+      fill('grey');
+      ellipse(width / 2, height / 2, 100, 100); // middle
+      ellipse(width / 2, height / 2 + 125, 100, 100); // bottom
+
+      break;
+  }
+
+
+}
--- p5/Week6Joke2/sketch.js
+++ p5/Week6Joke2/sketch.js
@@ -1,7 +1,34 @@
+et state = 0;
+
+
 function setup() {
-  // put setup code here
+  createCanvas(800, 800);
 }
 
 function draw() {
-  // put drawing code here
+
+  switch (state) {
+
+    case 0:
+      background('red');
+      text("What's really good?", 100, 100);
+      //for joke 2 put timer in here
+      break;
+
+    case 1: // punchline
+      background('green');
+      text("Idk but this code is hard", 100, 100);
+      break;
+
+  }
+
+}
+
+function mouseReleased() {
+
+  state = state + 1;
+  // if state is greater than one set equal to 0
+}
+if (state > 1) {
+  state = 0;
 }
