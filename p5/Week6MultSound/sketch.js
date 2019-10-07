16f62503b11750cd1755b0e58eb14d99d86771fe
Brandon Thomas <thomas_brandon77@yahoo.com>
2019-10-06 21:12:28 -0500

    truu

--- p5/Week6MultSound/sketch.js
+++ p5/Week6MultSound/sketch.js
@@ -1,6 +1,6 @@
         var song1, song2, song3, song4, song5;
         var myState = 0;
-        var myBColor;
+
 
         function preload() {
           song1 = loadSound('assets/YDBKhalid.mp3');
@@ -34,45 +34,62 @@
 
             case 0:
               song1.play();
-              text('So youre still thinkin bout me, just like I know you should. I can not give you everything you know I wish I could.', 50, 50);
-              textSize(36);
               myState = 1;
-              myBColor = color(random(255), random(255), random(255));
               break;
 
             case 1:
-              song2.play();
-              myBColor = color(random(255), random(255), random(255));
-              text('Thats why I love it, I love it, love it I love it, love it I love it. Thats why I love it, I love it, love it I love it, love it I love it.', 50, 50);
-
-              myState = 2;
-
-
-
+            background(0, 255, 0);
+              fill('orange');
+              textSize(20);
+              text('So youre still thinkin bout me, just like I know you should. I can not give you everything you know I wish I could.', 50, 50);
               break;
+
             case 2:
-              song3.play();
-              text('I was left to my own devices, many days fell away with nothing to show', 50, 50);
+              song2.play();
               myState = 3;
-              myBColor = color(random(255), random(255), random(255));
-
-
               break;
+
             case 3:
-              song4.play();
-              text('Haha, I get it down on the dance floor watch this, hey you dont even know girl come here', 50, 50);
-              myState = 4;
-              myBColor = color(random(255), random(255), random(255));
-
-
-
+              background(255, 0, 0);
+                fill('blue');
+              textSize(20);
+              text('Thats why I love it, I love it, love it I love it, love it I love it. Thats why I love it, I love it, love it I love it, love it I love it.', 50, 50);
               break;
 
             case 4:
+              song3.play();
+              myState = 5;
+              break;
+
+            case 5:
+              background(0, 255, 0);
+                fill('red');
+              textSize(20);
+              text('I was left to my own devices, many days fell away with nothing to show', 50, 50);
+              break;
+
+            case 6:
+              song4.play();
+              myState = 7;
+              break;
+
+            case 7:
+              fill('gold');
+              background(0, 255, 0);
+              textSize(20);
+              text('Haha, I get it down on the dance floor watch this, hey you dont even know girl come here', 50, 50);
+              break;
+
+            case 8:
               song5.play();
+              myState = 9;
+              break;
+
+            case 9:
+              background(0, 0, 255);
+                fill('purple');
+              textSize(20);
               text('Pour up, drank, head shot, drank sit down, drank, stand up, drank pass out, drank, wake up, drank faded, drank, faded, drank', 50, 50);
-              myState = 0;
-              myBColor = color(random(255), random(255), random(255));
               break;
 
           }
@@ -93,9 +110,4 @@
           if (myState > 5) {
             myState = 0;
           }
-        }
-
-
-        function touchStarted() {
-          getAudioContext().resume();
         }
